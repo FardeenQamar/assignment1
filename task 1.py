73e007e351877a6e128113d72966f7382fd8e29f
FardeenQamar <qamarfardeen@gmail.com>
2025-04-11 18:43:47 +0530

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,13 @@
+inp1 = float(input('Enter the first number : '))
+inp2 = float(input('Enter the second number (not zero) : '))
+
+sum = inp1 + inp2
+subt = inp1 - inp2
+mult = inp1 * inp2
+div = inp1 / inp2
+print(f'Addition = {sum}')
+print(f'Subtraction = {subt}')
+print(f'Multiplication = {mult}')
+print(f'Division = {div}')
+
+
---
+++ task 2.py
@@ -0,0 +1,4 @@
+First_name = input('Please type your first name : ')
+Last_name = input('Please type your last name : ')
+Greetings = f'Hello, {First_name} { Last_name}! Welcome to the Python program '
+print (Greetings)
